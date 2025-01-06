d1025de05fd082805914cd93931ba7b653d241df
dimsummarasigan <41361495+dimsummarasigan@users.noreply.github.com>
2025-01-06 20:20:20 +0800

    Create ImgurXHR.js

---
+++ ImgurXHR.js
@@ -0,0 +1,36 @@
+// USAGE YOU MUST HAVE INPUT ELEMENT
+// ADD THE INPUT ID IN THE FUNCTION PARAMETER
+// ADD THE OBJECT VARIABLE IN THE 2ND PARAMETER
+// THE OBJECT IMGLINK WILL HOLD THE LINK OF THE IMAGE BEING UPLOADED.
+
+
+var ImgurXHR = {
+    uploadImgUr: function (inputID, imgLink) {
+        document.getElementById(inputID).addEventListener('change', function (e) {
+            var file = e.target.files[0];
+            if (!file || !file.type.match(/image.*/))
+                return;
+
+            var fd = new FormData();
+            fd.append("image", file); // Append the file
+            var xhr = new XMLHttpRequest(); // Create the XHR (Cross-Domain XHR FTW!!!) Thank you sooooo much imgur.com
+            xhr.open("POST", "https://api.imgur.com/3/image.json"); // Boooom!
+            xhr.onload = function () {
+
+                if (xhr.status == 200) {
+                    imgLink.img = JSON.parse(xhr.responseText).data.link;
+                    // callback(img_file);
+                } else {
+                    window.alert('ImgurXHR error: Error in uploading... Please try again');
+                    imgLink.error = "Error Uploading in ImgUr";
+                    //callback("error in uploading...");
+                }
+            }
+            xhr.setRequestHeader('Authorization', 'Client-ID FOO');
+            xhr.send(fd);
+        }, false);
+    }
+}
+
+
+
